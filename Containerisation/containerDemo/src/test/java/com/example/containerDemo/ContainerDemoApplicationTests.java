d787da3af70e29074bc22750f9f20d21aa3b720d
Jake Peters <jake.peters@armakuni.com>
2022-07-12 13:10:59 +0100

    Added containerisation module

---
+++ Containerisation/containerDemo/src/test/java/com/example/containerDemo/ContainerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.containerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
