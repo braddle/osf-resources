e0231bcc3ebbf5efe2248c15069a648723192fae
Mark Bradley <braddle@gmail.com>
2022-11-18 14:15:13 +0000

    Completing the TDD lab - Leap Year Calculator

--- Testing/001-TDD/src/main/java/com.example.tdd/LeapYearCalculator.java
+++ Testing/001-TDD/src/main/java/com.example.tdd/LeapYearCalculator.java
@@ -1,4 +1,23 @@
 package com.example.tdd;
 
 public class LeapYearCalculator {
+    public boolean isLeapYear(int year) {
+        if (isDevisableBy(year, 400)) {
+            return true;
+        }
+
+        if (isDevisableBy(year, 100)) {
+            return false;
+        }
+
+        if (isDevisableBy(year, 4)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isDevisableBy(int year, int x) {
+        return year % x == 0;
+    }
 }
--- Testing/001-TDD/src/test/java/com.example.tdd/LeapYearCalculatorTest.java
+++ Testing/001-TDD/src/test/java/com.example.tdd/LeapYearCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.example.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -7,7 +8,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LeapYearCalculatorTest {
 
+    private LeapYearCalculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new LeapYearCalculator();
+    }
+
     @Test
-    void name() {
+    void YearsNotDivisibleBy4AreNot() {
+        assertFalse(calc.isLeapYear(2022));
+        assertFalse(calc.isLeapYear(2023));
+    }
+
+    @Test
+    void yearsDivisibleBy400AreLeapYears() {
+        assertTrue(calc.isLeapYear(400));
+        assertTrue(calc.isLeapYear(800));
+    }
+
+    @Test
+    void yearsDivisibleBy100ByNot400AreNotLeapYears() {
+        assertFalse(calc.isLeapYear(100));
+        assertFalse(calc.isLeapYear(500));
+    }
+
+    @Test
+    void YearsDivisibleBy4ButNot100AreLeapYears() {
+        assertTrue(calc.isLeapYear(4));
+        assertTrue(calc.isLeapYear(2136));
     }
 }
